a5c8104ab85e5c6fb078898aa7be049e21684efb
Romain0424 <60342445+Romain0424@users.noreply.github.com>
2021-12-02 08:40:18 +0100

    aDDING BATAILLE NAVALE

---
+++ bataille_navalle.py
@@ -0,0 +1,7 @@
+j1_mer1 = [[' ' for i in range(10)] for j in range(5)]
+j1_mer2 = [[' ' for i in range(10)] for j in range(5)]
+j1_mer3 = [[' ' for i in range(10)] for j in range(5)]
+j2_mer1 = [[' ' for i in range(10)] for j in range(5)]
+j2_mer2 = [[' ' for i in range(10)] for j in range(5)]
+j2_mer3 = [[' ' for i in range(10)] for j in range(5)]
+
