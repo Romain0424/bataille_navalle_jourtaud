d63b5778ec6d182b34acb3d02089f398a0cf3778
Romain0424 <60342445+Romain0424@users.noreply.github.com>
2021-12-02 08:47:47 +0100

    update

--- bataille_navalle.py
+++ bataille_navalle.py
@@ -1,7 +1,9 @@
-j1_mer1 = [[' ' for i in range(10)] for j in range(5)]
-j1_mer2 = [[' ' for i in range(10)] for j in range(5)]
-j1_mer3 = [[' ' for i in range(10)] for j in range(5)]
-j2_mer1 = [[' ' for i in range(10)] for j in range(5)]
-j2_mer2 = [[' ' for i in range(10)] for j in range(5)]
-j2_mer3 = [[' ' for i in range(10)] for j in range(5)]
+largeur = 10
+hauteur = 5
+j1_mer1 = [[' ' for i in range(hauteur)] for j in range(largeur)]
+j1_mer2 = [[' ' for i in range(hauteur)] for j in range(largeur)]
+j1_mer3 = [[' ' for i in range(hauteur)] for j in range(largeur)]
+j2_mer1 = [[' ' for i in range(hauteur)] for j in range(largeur)]
+j2_mer2 = [[' ' for i in range(hauteur)] for j in range(largeur)]
+j2_mer3 = [[' ' for i in range(hauteur)] for j in range(largeur)]
 
