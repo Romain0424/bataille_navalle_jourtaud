3a7b0bc643adc91fbeacad851aae390b4f2c49a7
Romain0424 <60342445+Romain0424@users.noreply.github.com>
2021-12-02 10:09:50 +0100

    affichage

--- bataille_navalle.py
+++ bataille_navalle.py
@@ -7,3 +7,17 @@ j2_mer1 = [[' ' for i in range(hauteur)] for j in range(largeur)]
 j2_mer2 = [[' ' for i in range(hauteur)] for j in range(largeur)]
 j2_mer3 = [[' ' for i in range(hauteur)] for j in range(largeur)]
 
+
+
+def afficher(mer):
+    for i in range(largeur):
+        for j in range(hauteur):
+            mer[i][j] = 'x'
+    print("mer 1 : \n")
+    print("    0   1   2   3   4   5   6   7   8   9 ")
+    l = ["A", "B", "C", "D", "E"]
+    for j in range(hauteur):
+        print(l[j] + "  ", end=" ")
+        for i in range(largeur):
+            print(mer[i][j] + " | ",end="")
+        print(" ")
